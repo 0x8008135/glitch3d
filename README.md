90352dd1a8fc083ef3ed1e259160fecb2b0603d3
0x800835 <karim.sudki@gmail.com>
2021-05-13 17:40:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Glitch3d #
+**THIS MODULE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND**
+
+This module offers basic functions to drive a 3D Marlin compatible printer through USB (e.g. to perform FI attacks).
+
+The master branch will be kept as generic as possible.
+
+Some printers may not behave identically, therefore branches with specific settings are available for fundamental differences.
+
+The module has been tested on the following models:
+- Creality Ender3 (ender3)
+- Creality 6 SE (master)
+
+## Example code for CR-6 SE##
+```
+from glitch3d import printer
+
+# Define Serial port settings
+serial_port = "/dev/ttyUSB0" # or set to None for automatic detection
+serial_port = None 
+serial_baud = 115200
+serial_timeout = 1
+
+# Establish connection with the printer
+p = printer(port=serial_port,baudrate=serial_baud, timeout=serial_timeout)
+
+# Define printer bed limits (mm)
+p.limits["min_x"] = 0.0
+p.limits["max_x"] = 235.0
+p.limits["min_y"] = 0.0
+p.limits["max_y"] = 235.0
+p.limits["min_z"] = 0.0
+p.limits["max_z"] = 250.0
+p.limits["min_s"] = 0.1
+p.limits["max_s"] = 100.0
+
+# Enter Manual mode
+p.manual()
+
+```
+
+## Manual mode ##
+In Manual mode, the following keystrokes are supported:
+
+* `up` Move probe forward / Y + step
+* `down` Move probe forward / Y - step
+* `right` Move probe right / X + step
+* `left` Move probe left / X - step 
+* `u` Raise probe / Z + step
+* `d` Lower probe / Z - step
+* `+` Increase step (0.1,1,10,100)
+* `-` Decrease step (100,10,1,0.1)
+* `s` Manual step definition
+* `h` Home on XY !!! WARNING printer dependant
+* `z` Home on XYZ 
+
+For safety reasons, a small routine was added to raise the probe of 20.0 mm before homing.
---
+++ printer.py
@@ -0,0 +1,229 @@
+# Copyright 2021 Karim Sudki
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import click
+import re
+
+import serial
+import serial.tools.list_ports
+import time
+
+class printer():
+    def __init__(self, port=None, baudrate=115200, timeout=None):
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+        self.s = 0.1
+
+        self.limits = {
+            "min_x" : 0.0,
+            "max_x" : 230.0,
+            "min_y" : 0.0,
+            "max_y" : 230.0,
+            "min_z" : 0.0,
+            "max_z" : 250.0,
+            "min_s" : 0.1,
+            "max_s" : 100.0,
+        }
+
+        if port is None:
+            for port in serial.tools.list_ports.comports():
+                if port.vid == 0x1A86 and port.pid== 0x7523:
+                    self.port = port.device
+        else:
+            self.port = port
+
+        try:
+            self._serialport = serial.Serial(self.port, baudrate=115200, timeout=None)
+        except serial.SerialException as e:
+            raise type(e)(f"Cannot send : {e.strerror}")
+        
+        print(self.read_until(b'\n0\n0'))
+        time.sleep(2)
+        self.get_pos()
+
+    def write(self, data=b""):
+        if not self.connected:
+            raise serial.SerialException("Not connected.")
+        try:
+            self._serialport.write(data+b"\r")
+            self._serialport.flush()
+        except serial.SerialException as e:
+            raise type(e)(f"Cannot send : {e.strerror}")
+
+    def read_until(self, c=None, size=None):
+        if not self.connected:
+            raise serial.SerialException("Not connected.")
+        try:
+            data = self._serialport.read_until(expected=c, size=None)
+            self._lastread = data
+            return data
+        except serial.SerialException as e:
+            raise type(e)(f"Cannot read : {e.strerror}")
+
+    def read(self, length=1):
+        if not self.connected:
+            raise serial.SerialException("Not connected.")
+        try:
+            data = self._serialport.read(length)
+            self._lastread = data
+            return data
+        except serial.SerialException as e:
+            raise type(e)(f"Cannot read : {e.strerror}")
+
+    def flush_input(self):
+        self._serialport.reset_input_buffer()
+
+    def close(self):
+        self._serialport.close()
+
+    def get_timeout(self):
+        return self._serialport.timeout
+
+    def set_timeout(self, value):
+        self._serialport.timeout = value
+
+    def connected(self):
+        return self._serialport.is_open
+
+    def set_unit(self,value):
+        if value == "mm":
+            self.write(b"G21")
+            self.read_until(c=b"ok")
+            print("Switching to Metric units (millimeters)")
+        elif value == "in":
+            self.write(b"G20")
+            self.read_until(c=b"ok")
+            print("Switching to US/Imperial units (inches)")
+
+    def go_home_xy(self):
+        self.check_limits(self.x, self.y, self.z+20, self.s)
+        self.set_pos(self.x, self.y, self.z+20)
+        self.write(b"G28 X Y")
+        self.read_until(c=b"ok")
+
+    def go_home_xyz(self):
+        self.check_limits(self.x, self.y, self.z+20, self.s)
+        self.set_pos(self.x, self.y, self.z+20)
+        self.write(b"G28 X Y Z")
+        self.read_until(c=b"ok")
+
+    def set_pos(self, x, y, z):
+        self.write(f"G0 X {x:.1f} Y {y:.1f} Z {z:.1f} F6000".encode())
+        self.read_until(c=b"ok")
+        self.x = x
+        self.y = y
+        self.z = z
+        self.get_pos()
+
+    def get_pos(self):
+        self.write(b"M114")
+        regex = re.compile(b'.*X:[-]*(?P<x>\d+.\d+)\sY:[-]*(?P<y>\d+.\d+)\sZ:[-]*(?P<z>\d+.\d+)\sE:')
+        m = re.search(regex,self.read_until(c=b"ok"))
+        if len(m.groups()) == 3:
+            self.x = float(m.group('x'))
+            self.y = float(m.group('y'))
+            self.z = float(m.group('z'))
+            print(f"X {self.x:.1f} | Y {self.y:.1f} | Z {self.z:.1f} | S {self.s:.1f}")
+            return self.x,self.y,self.z
+        else:
+            return self._serialport.timeout
+
+    def check_limits(self,x,y,z,s):
+        if self.limits["min_x"] <= x <= self.limits["max_x"]:
+            self.x = x
+        else:
+            print(f"X out of bounds ({self.limits['min_x']:.1f} to {self.limits['max_x']:.1f})")
+        if self.limits["min_y"] <= y <= self.limits["max_y"]:
+            self.y = y
+        else:
+            print(f"Y out of bounds ({self.limits['min_y']:.1f} to  {self.limits['max_y']:.1f})")
+        if self.limits["min_z"] <= z <= self.limits["max_z"]:
+            self.z = z
+        else:
+            print(f"Z out of bounds ({self.limits['min_z']:.1f} to {self.limits['max_z']:.1f})")
+        if self.limits["min_s"] <= s <= self.limits["max_s"]:
+            self.s = s
+        else:
+            print(f"Steps out of bounds ({self.limits['min_s']:.1f} to {self.limits['max_s']:.1f})")
+
+    def manual(self):
+        print("Entering Manual mode...")
+        self.get_pos()
+        while True:
+            c = click.getchar()
+            # ESC
+            if c == '\x1b':
+                click.echo('Exiting Manual mode...')
+                break
+            # Left Arrow
+            elif c == '\x1b[D':
+                new_x = self.x - self.s
+                self.check_limits(new_x, self.y, self.z, self.s)
+                self.set_pos(self.x, self.y, self.z)
+            # Right Arrow
+            elif c == '\x1b[C':
+                new_x = self.x + self.s
+                self.check_limits(new_x, self.y, self.z, self.s)
+                self.set_pos(self.x, self.y, self.z)
+            # Down Arrow
+            elif c == '\x1b[B':
+                new_y = self.y - self.s
+                self.check_limits(self.x, new_y, self.z, self.s)
+                self.set_pos(self.x, self.y, self.z)
+            # Up Arrow
+            elif c == '\x1b[A':
+                new_y = self.y + self.s
+                self.check_limits(self.x, new_y, self.z, self.s)
+                self.set_pos(self.x, self.y, self.z)
+            elif c == 'u':
+                new_z = self.z + self.s
+                self.check_limits(self.x, self.y, new_z, self.s)
+                self.set_pos(self.x, self.y, self.z)
+            elif c == 'd':
+                new_z = self.z - self.s
+                self.check_limits(self.x, self.y, new_z, self.s)
+                self.set_pos(self.x, self.y, self.z)
+            elif c == 's':
+                try:
+                    new_step = float(input("Enter desired step:"))
+                    self.check_limits(self.x, self.y, self.z, new_step)
+                except ValueError:
+                    print("Steps value must be a float")
+                    continue
+            elif c == '+':
+                if self.s < 1.0:
+                    self.s = 1.0
+                elif self.s < 10.0:
+                    self.s = 10.0
+                elif self.s < 100.0:
+                    self.s = 100.0
+            elif c == '-':
+                if self.s >= 100.0:
+                    self.s = 10.0
+                elif self.s >= 10.0:
+                    self.s = 1.0
+                elif self.s >= 1.0:
+                    self.s = 0.1
+            elif c == 'h':
+                if input("!!! WARNING !!! Printer will go directly to X/Y origins (a.k.a. Home) without passing GO!\r\nEnsure that nothing is still on the bed\r\nContinue (y/n)?") == "y":
+                    self.go_home_xy()
+                else:
+                    print("Canceled")
+            elif c == 'z':
+                if input("!!! WARNING !!! Printer will go directly to X/Y/Z origins without passing GO!\r\nEnsure that nothing is still on the bed\r\nContinue (y/n)?") == "y":
+                    self.go_home_xyz()
+                else:
+                    print("Canceled")
+            self.get_pos()
